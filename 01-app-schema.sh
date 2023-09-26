c7eebcbd8c8c53c73812779de45396545b121698
Brad Clawsie <brad@b7j0c.org>
2023-09-26 10:51:16 -0500

    push uuid and check into schema; breaking change up to 0.0.3

--- 01-app-schema.sh
+++ 01-app-schema.sh
@@ -2,65 +2,89 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "app" <<-EOSQL
+	create table if not exists model_base (
+		id uuid unique not null,
+		schema_version bigint not null default 0 check (schema_version >= 0 and schema_version <= 99999),
+	  status bigint not null default 0 check (status >= 0 and status <= 2),
+	  ctime bigint check (ctime >= 1672578000 and ctime <= 32503726800),
+	  mtime bigint check (mtime >= 1672578000 and ctime <= 32503726800),
+	  signature text unique not null,
+	  role bigint not null default 0 check (role >= 0 and role <= 2)
+	);
+
 	create table if not exists users (
-	       id text unique not null,
-	       api_secret text unique not null,
-	       api_secret_digest text unique not null,
-	       display_name text not null,
-	       display_name_digest text not null,
-	       email text not null,
-	       email_digest text not null,
-	       key_version text not null,
-	       org text not null,
-	       password text not null,
-	       schema_version bigint not null default 0,
-	       status bigint not null default 0,
-	       ctime bigint,
-	       mtime bigint,
-	       signature text unique not null,
-	       role bigint not null default 0,
-	       primary key (id));
+		-- our columns
+		api_secret uuid unique not null,
+	  api_secret_digest text unique not null,
+	  display_name text not null,
+	  display_name_digest text not null,
+	  email text not null check (email != ''),
+	  email_digest text not null check (email_digest != ''),
+	  key_version uuid not null,
+	  org uuid not null,
+	  password text not null check (password != ''),
+		-- model base
+		id uuid unique not null,
+		schema_version bigint not null default 0 check (schema_version >= 0 and schema_version <= 99999),
+	  status bigint not null default 0 check (status >= 0 and status <= 2),
+	  ctime bigint check (ctime >= 1672578000 and ctime <= 32503726800),
+	  mtime bigint check (mtime >= 1672578000 and ctime <= 32503726800),
+	  signature uuid unique not null,
+	  role bigint not null default 0 check (role >= 0 and role <= 2),
+		-- attributes
+	  primary key (id));
 
 	create unique index if not exists users_email_digest_org on users (email_digest, org);
 
 	create table if not exists orgs (
-	       id text unique not null,
-	       name text unique not null,
-	       owner text not null,
-	       schema_version bigint not null default 0,
-	       status bigint not null default 0,
-	       ctime bigint,
-	       mtime bigint,
-	       signature text unique not null,
-	       role bigint not null default 0,
-	       primary key (id));
+	  -- our columns
+	  name text unique not null check (name != ''),
+	  owner uuid not null,
+		-- model base
+		id uuid unique not null,
+		schema_version bigint not null default 0 check (schema_version >= 0 and schema_version <= 99999),
+	  status bigint not null default 0 check (status >= 0 and status <= 2),
+	  ctime bigint check (ctime >= 1672578000 and ctime <= 32503726800),
+	  mtime bigint check (mtime >= 1672578000 and ctime <= 32503726800),
+	  signature uuid unique not null,
+	  role bigint not null default 0 check (role >= 0 and role <= 2),
+		-- attributes
+	  primary key (id));
 
 	create table if not exists repositories (
-	       id text unique not null,
-	       name text not null,
-	       org text not null,
-	       path text not null,
-	       upstream text not null,
-	       schema_version bigint not null default 0,
-	       status bigint not null default 0,
-	       ctime bigint,
-	       mtime bigint,
-	       signature text unique not null,
-	       role bigint not null default 0,
-	       primary key (id));
+	  -- our columns
+	  name text not null check (name != ''),
+	  org uuid not null,
+	  path text not null check (path != ''),
+	  upstream text not null check (path != ''),
+		-- model base
+		id uuid unique not null,
+		schema_version bigint not null default 0 check (schema_version >= 0 and schema_version <= 99999),
+	  status bigint not null default 0 check (status >= 0 and status <= 2),
+	  ctime bigint check (ctime >= 1672578000 and ctime <= 32503726800),
+	  mtime bigint check (mtime >= 1672578000 and ctime <= 32503726800),
+	  signature uuid unique not null,
+	  role bigint not null default 0 check (role >= 0 and role <= 2),
+		-- attributes
+	  primary key (id));
 
 	create unique index if not exists repositories_name_org on repositories (name, org);
 
 	create table if not exists audit (
-	       id text unique not null,
-	       code bigint not null,
-	       source text not null,
-	       source_id text not null,
-	       schema_version bigint not null default 0,
-	       ctime bigint,
-	       mtime bigint,
-	       signature text,
-	       primary key (id));
+	  -- our columns
+	  code bigint not null check (code >= 0 and code <= 99999),
+	  source text not null check (source != ''),
+	  source_id uuid not null,
+		-- model base
+		id uuid unique not null,
+		schema_version bigint not null default 0 check (schema_version >= 0 and schema_version <= 99999),
+	  status bigint not null default 0 check (status >= 0 and status <= 2),
+	  ctime bigint check (ctime >= 1672578000 and ctime <= 32503726800),
+	  mtime bigint check (mtime >= 1672578000 and ctime <= 32503726800),
+	  signature uuid unique not null,
+	  role bigint not null default 0 check (role >= 0 and role <= 2),
+		-- attributes
+	  primary key (id));
 
 	create or replace function on_model_insert()
 	 returns trigger
@@ -72,7 +96,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "app" <<-EOSQL
 	  _new := NEW;
 	  _new."ctime" = floor(extract(epoch from now()));
 	  _new."mtime" = floor(extract(epoch from now()));
-	  _new."signature" = cast(gen_random_uuid() as text);
+	  _new."signature" = gen_random_uuid();
 	  return _new;
 	end;
 	\$ctime_mtime_set\$;
@@ -86,7 +110,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "app" <<-EOSQL
 	begin
 	  _new := NEW;
 	  _new."mtime" = floor(extract(epoch from now()));
-	  _new."signature" = cast(gen_random_uuid() as text);
+	  _new."signature" = gen_random_uuid();
 	  return _new;
 	end;
 	\$mtime_set\$;
